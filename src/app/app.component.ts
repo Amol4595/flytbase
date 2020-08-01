d7d3a86d97ebb366ceae06621524a6d8e623bc74
vandana rani <mobilityhelpdesk.mm@gmail.com>
2020-08-02 01:28:19 +0530

    'sec'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { HostListener } from '@angular/core';
 
 @Component({
@@ -13,44 +13,79 @@ export class AppComponent {
   selectedArrId:any;
   addBoxCount:number=0;
   keyboardOn: boolean = true;
+  dif:any;
 
   constructor(){}
 
-  @HostListener('window:keyup', ['$event'])
+  @HostListener('window:keydown', ['$event'])
   handleKeyDown(event: KeyboardEvent) {
     //console.log('event.key',event.key,this.keyboardOn);
     if(this.keyboardOn){
+      let ele = document.getElementById(this.selectedDivId);
+      let left = parseInt(ele.style.left);
+      left=left>0?left:0;
+      let top = parseInt(ele.style.top);
+      top=top>0?top:0;
       switch(event.key) {
         case 'Delete':  
           this.deleteBox()  
           break;
         case 'ArrowLeft':
-          this.comFun(-1);
+          if(left>0){
+            this.dif = left - 1;
+            ele.style.left = this.dif +'px';
+          }
           break;
         case 'ArrowRight':
-          this.comFun(1);
+          //this.comFun(1);
+          if(left<490){
+            this.dif = left + 1;
+            ele.style.left = this.dif +'px';
+          }
           break;
         case 'ArrowUp':
-          this.comFun(-5);
+          //this.comFun(-5);
+          if(top>0){
+            this.dif = top - 1;
+            ele.style.top = this.dif + 'px';
+          }
           break;
         case 'ArrowDown':
-          this.comFun(5)
+          //this.comFun(5)
+          if(top<490){
+            this.dif = top + 1;
+            ele.style.top = this.dif + 'px';
+          }
           break;
         case 'a':
-          this.comFun(-1);
+          if(left>0){
+            this.dif = left - 1;
+            ele.style.left = this.dif +'px';
+          }
           break;
         case 'd':
-          this.comFun(1);
+          //this.comFun(1);
+          if(left<490){
+            this.dif = left + 1;
+            ele.style.left = this.dif +'px';
+          }
           break;
         case 'w':
-          this.comFun(-5);
+          if(top>0){
+            this.dif = top - 1;
+            ele.style.top = this.dif + 'px';
+          }
           break;
         case 's':
-          this.comFun(5)
+          if(top<490){
+            this.dif = top + 1;
+            ele.style.top = this.dif + 'px';
+          }
           break;
         default:
           // code block
       }
+      
     }
   }
 
@@ -62,20 +97,18 @@ export class AppComponent {
     this.addBoxCount = this.addBoxCount + 1;
     let Rndid = '_' + Math.random().toString(36).substr(2, 9);
     this.boxArr.push({id:Rndid,zIndex:this.addBoxCount})
-    //console.log('boxArr',this.boxArr);
   }
 
   deleteBox(){
-    //console.log('this.selectedArrId',this.boxArr,this.selectedArrId)
     this.boxArr.splice(this.selectedArrId, 1);
   }
 
   comFun(decInBy){
-    let targetIndex = this.selectedArrId+decInBy;
-    if(this.boxArr[targetIndex]){
-      this.selectedDivId = this.boxArr[targetIndex].id;
-      this.selectedArrId = targetIndex;
-    }
+    // let targetIndex = this.selectedArrId+decInBy;
+    // if(this.boxArr[targetIndex]){
+    //   this.selectedDivId = this.boxArr[targetIndex].id;
+    //   this.selectedArrId = targetIndex;
+    // }
   }
 
   onboxSelect(data,e,i){
